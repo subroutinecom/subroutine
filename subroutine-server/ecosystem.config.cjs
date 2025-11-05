6d6f8bd70dc19ab20f2a5bed127a77beb8930af5
Greg Pstrucha <875316+Gricha@users.noreply.github.com>
2025-11-05 09:44:36 -0800

    Subroutine server (#5)

---
+++ subroutine-server/ecosystem.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  exp_backoff_restart_delay: 100,
+  apps: [
+    {
+      name: "subroutine-server",
+      script: "deno",
+      args: "task dev",
+      autorestart: true,
+
+      watch: ["*.tsx", "*.ts", "*.js", "*.json", "app/**/*.tsx", "app/**/*.ts"],
+
+      ignore_watch: ["node_modules", ".git"],
+
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+    {
+      name: "linter",
+      script: "deno",
+      args: "lint",
+      autorestart: false,
+
+      watch: ["*.tsx", "*.ts", "app/**/*.tsx", "app/**/*.ts"],
+
+      ignore_watch: ["node_modules", ".git"],
+    },
+  ],
+};
