04c0a63d92892cf2a36607fd89f21a490e9386ca
Greg Pstrucha <875316+Gricha@users.noreply.github.com>
2025-11-08 00:17:19 +0000

    Set up sandbox with ability to build prod build

--- sandbox/manage.sh
+++ sandbox/manage.sh
@@ -5,6 +5,10 @@ set -e
 do_run() {
   deno install
 
+  # print env
+  echo "Environment:"
+  env
+
   echo "Starting PM2 processes..."
   pm2 start ecosystem.config.js
 
@@ -12,8 +16,15 @@ do_run() {
   pm2 logs
 }
 
+do_build() {
+  echo "Building"
+  deno install
+}
+
 if [ "$1" = "--run" ]; then
   do_run
+elif [ "$1" = "--build" ]; then
+  do_build
 else
   echo "No command selected."
 fi
