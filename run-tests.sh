da1839c4dab8f50252f42d6db3d5deb5cb13de3e
Jeremy Stanley <git@azulus.com>
2025-12-01 13:24:31 -0800

    .

---
+++ run-tests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "${SCRIPT_DIR}"
+
+docker compose exec -e TEST_NAMES tests ./manage.sh --test "$@"
