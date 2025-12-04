2d39f65b59d537aa65e70c6e3fe2afc980da4e77
Jeremy Stanley <git@azulus.com>
2025-12-04 09:36:26 -0800

    checks

--- eslint.config.ts
+++ eslint.config.ts
@@ -47,11 +47,16 @@ export default tseslint.config([
       "@typescript-eslint/no-unused-expressions": 0,
       "@typescript-eslint/no-unused-vars": 0,
 
-      "local/no-console": "error",
       "local/no-console-without-text": "error",
       "local/no-routes-imports": "error",
       "local/no-window-session-or-local-storage": "error",
       "local/only-fieldWithInput-mutations": "error",
+    },
+  },
+  {
+    files: ["api/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    rules: {
+      "local/no-console": "error",
     },
   },
 ]);
